f2295b9d699756ee5e6681622aa8ea8dfc8eef45
Mooner510 <lmck510@gmail.com>
2022-08-13 17:30:45 +0900

    Save The Map

--- src/main/java/org/mooner/disablechorusfruit/DisableChorusFruit.java
+++ src/main/java/org/mooner/disablechorusfruit/DisableChorusFruit.java
@@ -1,6 +1,11 @@
 package org.mooner.disablechorusfruit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DisableChorusFruit extends JavaPlugin implements Listener {
@@ -9,11 +14,29 @@ public final class DisableChorusFruit extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Plugin Enabled!");
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Plugin Disabled!");
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent e) {
+        if (e.getCause() == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT || e.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onExplode(BlockExplodeEvent e) {
+        e.blockList().clear();
+    }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent e) {
+        e.blockList().clear();
     }
 }
